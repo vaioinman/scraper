32b15c024b20ec1a23003d2a0aa25b9c87bf63ee
Carl Tran <ctran@sky.com>
2018-11-10 12:19:17 +0000

    Added run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+mvn spring-boot:run -Dspring-boot.run.arguments="https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html"
